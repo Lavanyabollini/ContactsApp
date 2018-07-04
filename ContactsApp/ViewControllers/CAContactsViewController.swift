34bcdf1c40237d37612084beda71b800c7341323
Lavanya <bollini.lavanya@mobinius.com>
2018-07-04 17:48:07 +0530

    Code changes for Fixes

--- ContactsApp/ViewControllers/CAContactsViewController.swift
+++ ContactsApp/ViewControllers/CAContactsViewController.swift
@@ -65,15 +65,7 @@ class CAContactsViewController: UIViewController,UITableViewDelegate,UITableView
         }
         return cell
     }
-     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if (editingStyle == UITableViewCellEditingStyle.delete) {
-            // handle delete (by removing the data from your array and updating the tableview)
-        }
-    }
+
     //MARK:- IBAction methods
     @IBAction func addContact(_ sender: Any) {
     
