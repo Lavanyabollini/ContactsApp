1425b2a3f4ef9aa565cff35d2e1b9e77e8e8ef03
Lavanya <bollini.lavanya@mobinius.com>
2018-07-06 18:37:19 +0530

    code chnages for UI fixes and implemnted deletingcontact from list

---
+++ ContactsApp/New Group/SharedManager.swift
@@ -0,0 +1,13 @@
+//
+//  SharedManager.swift
+//  ContactsApp
+//
+//  Created by Lavanya on 06/07/18.
+//  Copyright © 2018 Lavanya. All rights reserved.
+//
+
+import UIKit
+
+class SharedManager: NSObject {
+
+}
